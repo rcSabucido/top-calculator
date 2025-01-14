6153477b0f68fa71b4d42de6f82abc4c7e14cbda
rcSabucido <rcsabucido@gmail.com>
2025-01-14 16:19:51 +0800

    Add num button functionality in order to interact with the display, also center the text inside each button

--- app.js
+++ app.js
@@ -35,3 +35,14 @@ function operate(a, b, operand) {
 let num1 = 0;
 let operand = "";
 let num2 = 0;
+const numBtns = document.querySelectorAll(".num-btns");
+const displayContainer = document.querySelector(".bottom-display");
+let accumulate = "";
+
+numBtns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        accumulate += event.target.textContent;
+        displayContainer.textContent = accumulate;
+    });
+})
+
