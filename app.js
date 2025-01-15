bf48be28a710c402dbd67483b078a627a31e31f8
rcSabucido <rcsabucido@gmail.com>
2025-01-15 16:05:17 +0800

    Add top div to display to handle operator input visualization

--- app.js
+++ app.js
@@ -36,13 +36,24 @@ let num1 = 0;
 let operand = "";
 let num2 = 0;
 const numBtns = document.querySelectorAll(".num-btns");
-const displayContainer = document.querySelector(".bottom-display");
+const opBtns = document.querySelectorAll(".op-btns");
+const bottomDisplay = document.querySelector(".bottom-display");
+const topDisplay = document.querySelector(".top-display");
 let accumulate = "";
+let topCurrent = "";
 
 numBtns.forEach((btn) => {
     btn.addEventListener("click", (event) => {
         accumulate += event.target.textContent;
-        displayContainer.textContent = accumulate;
+        bottomDisplay.textContent = accumulate;     
     });
+})
+
+opBtns.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+        topCurrent += accumulate + event.target.textContent;
+        topDisplay.textContent = topCurrent;
+        accumulate = "";
+    })
 })
 
